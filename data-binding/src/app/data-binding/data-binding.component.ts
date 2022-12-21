2f5bf0deed11575ca50354e52253111ca45e959e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-12-21 15:54:38 -0300

    event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,12 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css'],
+  // styleUrls: ['./data-binding.component.css'], ou
+  styles: [
+    `
+      .highlight {
+        background-color: green;
+        font-weight: bold;
+      }
+    `,
+  ],
 })
 export class DataBindingComponent {
   url: string = 'https://loiane.training';
   cursoAngular: boolean = true;
   urlImagem: string = 'https://picsum.photos/400/200';
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   getValor() {
     return 1;
@@ -16,5 +28,21 @@ export class DataBindingComponent {
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Clicou no botão');
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  salvarValor(value: string) {
+    this.valorSalvo = value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 }
