752fb3d5af5ca7aee06edcb227fa2faf5c497a19
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-12-23 18:33:41 -0300

    diretiva *ngSwitch

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { TestExternalLibsComponent } from './test-external-libs/test-external-libs.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     TestExternalLibsComponent,
+    DiretivaNgswitchComponent,
   ],
   imports: [BrowserModule, BrowserAnimationsModule, MatExpansionModule],
   providers: [],
---
+++ diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngswitch',
+  templateUrl: './diretiva-ngswitch.component.html',
+  styleUrls: ['./diretiva-ngswitch.component.scss'],
+})
+export class DiretivaNgswitchComponent {
+  aba: string = 'home';
+}
