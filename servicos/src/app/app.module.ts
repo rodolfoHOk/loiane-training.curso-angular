160da5ba97bf80d5d0f4416cb5570a7d0599cb4d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2022-12-26 14:59:31 -0300

    injetando um serviço em outros serviços

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './services/log.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CriarCursoModule, CursosModule],
+  providers: [LogService],
   // providers: [CursosService],
   bootstrap: [AppComponent],
 })
