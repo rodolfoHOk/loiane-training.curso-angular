34b28c79c1a8b602a79073ae8e2880d44073ff6e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-01-12 20:26:09 -0300

    forms reativos criando form com código angular

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
   styleUrls: ['./data-form.component.scss'],
 })
-export class DataFormComponent {
-  formulario?: FormGroup;
+export class DataFormComponent implements OnInit {
+  formulario!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    // this.formulario = new FormGroup({
+    //   nome: new FormControl(''),
+    //   email: new FormControl(''),
+    // });
+
+    this.formulario = this.formBuilder.group({
+      nome: [''],
+      email: [''],
+    });
+  }
 }
