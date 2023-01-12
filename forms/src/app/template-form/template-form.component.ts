0a236cac1f6308a1c58b73546165c34f5aa44d19
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-01-11 23:38:56 -0300

    forms template driven populando campos com setValue e patchValue

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Endereco } from './endereco';
 
 @Component({
   selector: 'app-template-form',
@@ -11,6 +13,8 @@ export class TemplateFormComponent {
     nome: '',
     email: '',
   };
+
+  constructor(private http: HttpClient) {}
 
   onSubmit(form: NgForm) {
     console.log(form);
@@ -26,5 +30,63 @@ export class TemplateFormComponent {
     }
 
     return false;
+  }
+
+  consultaCEP(event: FocusEvent, form: NgForm) {
+    const inputElement = event.target as HTMLInputElement;
+    let cep = inputElement.value.replace(/\D/g, '');
+
+    if (cep !== '') {
+      let validaCep = /^[0-9]{8}$/;
+
+      if (validaCep.test(cep)) {
+        this.resetDadosForm(form);
+
+        let url = `https://viacep.com.br/ws/${cep}/json`;
+
+        this.http.get<Endereco>(url).subscribe((dados) => {
+          this.populaDadosForm(dados, form);
+        });
+      }
+    }
+  }
+
+  private populaDadosForm(dados: Endereco, form: NgForm) {
+    // form.setValue({
+    //   nome: form.value.nome,
+    //   email: form.value.email,
+    //   endereco: {
+    //     cep: dados.cep,
+    //     numero: form.value.endereco.numero,
+    //     complemento: dados.complemento,
+    //     logradouro: dados.logradouro,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf,
+    //   },
+    // });
+
+    form.form.patchValue({
+      endereco: {
+        cep: dados.cep,
+        complemento: dados.complemento,
+        logradouro: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      },
+    });
+  }
+
+  private resetDadosForm(form: NgForm) {
+    form.form.patchValue({
+      endereco: {
+        complemento: '',
+        logradouro: '',
+        bairro: '',
+        cidade: '',
+        estado: '',
+      },
+    });
   }
 }
