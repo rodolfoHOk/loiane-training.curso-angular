5059aebceaeb69f4d3d8d221bcb12d416a574a36
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-01-24 20:44:00 -0300

    http alerta de erros com bootstrap

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { catchError, EMPTY, Observable, Subject } from 'rxjs';
+import { AlertModalComponent } from 'src/app/shared/alert-modal/alert-modal.component';
 import { Curso } from '../cursos';
 import { CursosService } from '../cursos.service';
 
@@ -11,8 +13,12 @@ import { CursosService } from '../cursos.service';
 export class CursosListaComponent implements OnInit {
   cursos$?: Observable<Curso[]>;
   error$ = new Subject<boolean>();
+  bsModalRef?: BsModalRef;
 
-  constructor(private service: CursosService) {}
+  constructor(
+    private service: CursosService,
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit(): void {
     this.onRefresh();
@@ -22,20 +28,17 @@ export class CursosListaComponent implements OnInit {
     this.cursos$ = this.service.list().pipe(
       catchError((error) => {
         console.error(error);
-        this.error$.next(true);
+        // this.error$.next(true);
+        this.handleError();
         return EMPTY;
       })
     );
+  }
 
-    // Outra maneira de capturar erros:
-    // this.service.list().subscribe({
-    //   next: (dados) => {
-    //     console.log(dados);
-    //   },
-    //   error: (error) => {
-    //     console.error(error);
-    //   },
-    //   complete: () => console.log('Observable completo'),
-    // });
+  handleError() {
+    this.bsModalRef = this.modalService.show(AlertModalComponent);
+    this.bsModalRef.content.type = 'danger';
+    this.bsModalRef.content.message =
+      'Erro ao carregar cursos. Tente novamente mais tarde.';
   }
 }
