ae6933070a273b2b35b0f7bf8e8b7df119322487
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-01-26 18:36:04 -0300

    upload de arquivo barra de progresso + observando eventos http

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,3 +1,4 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -10,6 +11,7 @@ import { UploadFileService } from '../upload-file.service';
 })
 export class UploadFileComponent implements OnDestroy {
   files: Set<File> = new Set();
+  progress: number = 0;
 
   sub?: Subscription;
 
@@ -26,13 +28,24 @@ export class UploadFileComponent implements OnDestroy {
     for (let i = 0; i < selectedFiles.length; i++) {
       this.files.add(selectedFiles[i]);
     }
+
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files.size > 0) {
       this.sub = this.service
         .upload(this.files, `${environment.BASE_URL}/upload`)
-        .subscribe((response) => console.log('Upload concluído', response));
+        .subscribe((event: HttpEvent<Object>) => {
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log('Upload concluído');
+          } else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total!);
+            console.log('Progresso:', percentDone);
+            this.progress = percentDone;
+          }
+        });
     }
   }
 }
