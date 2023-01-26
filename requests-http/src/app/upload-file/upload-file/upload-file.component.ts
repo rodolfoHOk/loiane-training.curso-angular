a00b5f152ab75602597cfb76f2aed0e135fc8afd
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-01-26 17:40:53 -0300

    upload de arquivo Request com FormData

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.scss'],
 })
-export class UploadFileComponent {
+export class UploadFileComponent implements OnDestroy {
+  files: Set<File> = new Set();
+
+  sub?: Subscription;
+
+  constructor(private service: UploadFileService) {}
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
   onChange(event: Event) {
     console.log(event);
     const selectedFiles: FileList = (<HTMLInputElement>event.target).files!;
-    console.log(selectedFiles);
+
+    for (let i = 0; i < selectedFiles.length; i++) {
+      this.files.add(selectedFiles[i]);
+    }
+  }
+
+  onUpload() {
+    if (this.files.size > 0) {
+      this.sub = this.service
+        .upload(this.files, 'http://localhost:8000/upload')
+        .subscribe((response) => console.log('Upload concluído', response));
+    }
   }
 }
